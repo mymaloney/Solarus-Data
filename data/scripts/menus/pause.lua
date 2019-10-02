4875aa9eb5b63fc872eb036ab66d01a692c59d4f
Colin Crovella <ccc5nh@virginia.edu>
2019-10-01 20:49:38 -0400

    First implementation of pause, from ALttP

--- data/scripts/menus/pause.lua
+++ data/scripts/menus/pause.lua
@@ -1,79 +1,140 @@
+-- Script that creates a pause menu for a game.
+
+-- Usage:
+-- require("scripts/menus/pause")
+
 require("scripts/multi_events")
 
-local width,height = sol.video.get_quest_size()
-local pause_screen_surface = sol.surface.create(width,height)
+-- Creates a pause menu for the specified game.
+local function initialize_pause_features(game)
 
-local function initialize_pause_features(game)  
-  
-  local inventory_builder = nil  
-  local inventory_submenu = nil  
-  
   if game.pause_menu ~= nil then
+    -- Already done.
     return
-  end  
-  
-  local pause_menu = {}
-  
-  function pause_menu:on_started()
-    inventory_builder = require("scripts/menus/pause_inventory")
-    inventory_submenu = inventory_builder:new(game)
-    pause_screen_surface = sol.surface.create(width,height)
   end
-    
-  function pause_menu:on_finished()
 
-  end  
-  
-  --Called to open the pause menu
+  local inventory_builder = require("scripts/menus/pause_inventory")
+  local commands_builder = require("scripts/menus/pause_commands")
+
+  local pause_menu = {}
+  game.pause_menu = pause_menu
+  local pause_submenus
+  local submenu_index
+
+  local function set_submenu_index(index)
+
+    if pause_submenus[submenu_index] ~= nil then
+      sol.menu.stop(pause_submenus[submenu_index])
+    end
+    submenu_index = index
+    game:set_value("pause_last_submenu", index)
+    sol.menu.start(pause_menu, pause_submenus[index], false)
+  end
+
+  local function get_submenus_order()
+
+    local order = {
+      inventory = 1,
+      commands = 2,
+    }
+    return order
+  end
+
+  function pause_menu:on_started()
+
+    -- Define the available submenus and their order.
+    local submenus_order = get_submenus_order()
+
+    -- Array of submenus (inventory, etc.).
+    pause_submenus = {}
+    pause_submenus[submenus_order.inventory] = inventory_builder:new(game)
+    pause_submenus[submenus_order.commands] = commands_builder:new(game)
+
+    -- Play the sound of pausing the game.
+    sol.audio.play_sound("pause_open")
+
+    -- Show the inventory initially.
+    set_submenu_index(submenus_order.inventory)
+  end
+
+  function pause_menu:on_finished()
+    pause_submenus = nil
+  end
+
+  function pause_menu:next_submenu()
+
+    sol.audio.play_sound("pause_closed")
+    set_submenu_index((submenu_index % #pause_submenus) + 1)
+  end
+
+  function pause_menu:previous_submenu()
+
+    sol.audio.play_sound("pause_closed")
+    set_submenu_index((submenu_index - 2) % #pause_submenus + 1)
+  end
+
+  function pause_menu:switch_submenu(submenu_name)
+
+    local submenus_order = get_submenus_order()
+    local index = submenus_order[submenu_name]
+    if index == nil then
+      return
+    end
+    if game:is_paused() and submenu_index == index then
+      -- Already the active one: close it.
+        sol.audio.play_sound("pause_closed")
+        game:set_paused(false)
+    else
+      -- Open the specified submenu.
+      if not game:is_paused() then
+        game:set_paused(true)
+      else
+        sol.audio.play_sound("pause_closed")
+      end
+      if submenu_index ~= index then
+        set_submenu_index(index)
+      end
+    end
+  end
+
   function pause_menu:open()
-    pause_menu:move_pause_screen(function() end)
     sol.menu.start(game, pause_menu)
   end
-  
-  --Called to close the pause menu
+
   function pause_menu:close()
-    pause_menu:move_pause_screen(function() end)
     sol.menu.stop(pause_menu)
   end
-  
-  
-  function pause_menu:move_pause_screen(callback)
-    local angle_added = math.pi
-    local movement_speed = 1    
-    local movement_distance = height
-    
-    if (false) then
-      -- Opposite direction when closing.
-      angle_added = 0
-    end
 
-    local movement = sol.movement.create("straight")
-    movement:set_speed(movement_speed)
-    movement:set_max_distance(movement_distance)
-    movement:set_angle(3 * math.pi / 2 + angle_added)
-    movement:start(pause_screen_surface, callback)
-  end  
-  
-  
-  
-  function pause_menu:on_draw(dst_surface)
-    if (inventory_submenu ~= nil) then
-      pause_screen_surface:clear()
-      inventory_submenu:draw_grid(pause_screen_surface, 10, 10)
-      pause_screen_surface:draw(dst_surface)
+  function pause_menu:on_command_pressed(command)
+
+    local handled = false
+    if command == "left" and #pause_submenus > 1 then
+      pause_menu:previous_submenu()
+      handled = true
+    elseif command == "right" and #pause_submenus > 1 then
+      pause_menu:next_submenu()
+      handled = true
+    elseif command == "pause" then
+      sol.audio.play_sound("pause_closed")
+      game:set_paused(false)
+      handled = true
     end
+    return handled
   end
-  
-  --Adds pause_menu:open() and pause_menu:close() to their respective events in game,
-  --so that when the game is paused/unpaused, it calls pause_menu:open()/close()
+
+  -- Pauses the game with the specified pause submenu,
+  -- or unpauses the game if this submenu is already active.
+  function game:switch_pause_menu(submenu_name)
+    pause_menu:switch_submenu(submenu_name)
+  end
+
   game:register_event("on_paused", function(game)
     pause_menu:open()
   end)
   game:register_event("on_unpaused", function(game)
     pause_menu:close()
   end)
-  
-  game.pause_menu = pause_menu
+
 end
 
 -- Set up the pause menu on any game that starts.
--- data/scripts/menus/pause_inventory.lua
+++ data/scripts/menus/pause_inventory.lua
@@ -1,60 +1,294 @@
 local inventory_manager = {}
-inventory_manager.grid = {}
-inventory_manager.sprites_grid = {}
 
-local num_rows = 5
-local num_columns = 2
-local size = 16  --Size of items in the grid, in pixels (length of one side of square)
+local gui_designer = require("scripts/menus/lib/gui_designer")
+
+local inventory_items_names = {
+  
+}
+
+local main_equipment_names = {
+    
+}
+
+local secondary_equipment_names = {
+  
+}
+
+local quest_items_name = {
+  
+}
+
+-- The inventory's numbers of columns and rows of are used for the selection system
+local inventory_num_columns = 5
+local inventory_num_rows = math.ceil(#inventory_items_names / inventory_num_columns)
+
+local movement_speed = 800
+local movement_distance = 160
+
+local function display_items_grid(game, widget, item_names, item_script_folder, sprite_folder, num_columns, x_margin, x_padding, y_margin, y_padding)
+  -- Draw the items, if they are possessed, depending on their variant
+  for i, item_name in ipairs(item_names) do    
+    local variant = game:get_item(item_script_folder .. "/" .. item_name):get_variant() -- 0 = not possesed
+
+    if variant > 0 then
+      local column = (i - 1) % num_columns + 1
+      local row = math.floor((i - 1) / num_columns + 1) - 1
+      -- Draw the sprite statically. This is okay as long as
+      -- item sprites are not animated.
+      -- If they become animated one day, they will have to be
+      -- drawn at each frame instead (in on_draw()).
+      local item_sprite = sol.sprite.create(sprite_folder .. "/" .. item_name)
+      item_sprite:set_direction(variant - 1)
+      item_sprite:set_xy(x_margin + (column * (16 + x_padding)) - 16, y_margin + (row * (16 + y_padding)))
+      item_sprite:draw(widget:get_surface())
+    end
+  end
+end
+
+-- Draw part of inventory containing the equipable items
+local function create_inventory_widget(game)
+  local widget = gui_designer:create(176, 144)
+  widget:set_xy(16, 16 - movement_distance)
+  widget:make_green_frame()
+
+  display_items_grid(game, widget, inventory_items_names, "inventory", "menus/items/inventory", inventory_num_columns, 8, 16, 29, 16)
+
+  return widget
+end
+
+local function create_quest_widget(game)
+
+  local widget = gui_designer:create(112, 144)
+  widget:set_xy(200, 16 - movement_distance)
+  widget:make_yellow_frame()
+  local num_columns = 3
+  
+  display_items_grid(game, widget, quest_items_name, "quest", "menus/items/quest", num_columns, 8, 16, 24, 8)
+
+  return widget
+
+end
+
+local function create_equipment_widget(game)
+
+  local widget = gui_designer:create(112, 64)
+  widget:set_xy(200, 168 - movement_distance)
+  widget:make_yellow_frame()
+  local items_surface = widget:get_surface()
+  local num_columns = 3
+  
+  display_items_grid(game, widget, main_equipment_names, "equipment", "menus/items/equipment", num_columns, 8, 16, 24, 8)
+  
+  return widget
+  
+end
+
+local function create_do_widget(game)
+
+  local widget = gui_designer:create(176, 64)
+  widget:set_xy(16, 168 - movement_distance)
+  widget:make_red_frame()
+  local num_columns = 5
+  
+  display_items_grid(game, widget, secondary_equipment_names, "equipment", "menus/items/equipment", num_columns, 8, 16, 24, 8)
+  
+  return widget
+
+end
 
 function inventory_manager:new(game)
-  --Initializing the matrix of item names
-  for i=1,num_rows do
-    self.grid[i] = {}
-    for j=1,num_columns do
-      self.grid[i][j] = nil
-    end
-  end
-  
-  --Adding item names to the matrix of item names
-  --TODO: make it so that it only adds items which are in the player's inventory.
-  self.grid[1][1] = "bombs"
-  self.grid[1][2] = "bow"
-  
-  self:initialize_sprites_grid()
-  
-  --returning the inventory_manager
-  return self
-end
 
-function inventory_manager:draw_grid(dst_surface, x, y)
-  for i=1,num_rows do
-    for j=1,num_columns do
-      if (self.sprites_grid[i][j] ~= nil) then
-        (self.sprites_grid[i][j]):draw(dst_surface, x+((i-1)*size), y+((j-1)*size))
+  local inventory = {}
+
+  local state = "opening"  -- "opening", "ready" or "closing".
+
+  local inventory_widget = create_inventory_widget(game)
+  local quest_widget = create_quest_widget(game)
+  local do_widget = create_do_widget(game)
+  local equipment_widget = create_equipment_widget(game)
+
+  local item_cursor_fixed_sprite = sol.sprite.create("menus/item_cursor")
+  item_cursor_fixed_sprite:set_animation("solid_fixed")
+  local item_cursor_moving_sprite = sol.sprite.create("menus/item_cursor")
+  item_cursor_moving_sprite:set_animation("dashed_blinking")
+
+  -- Determine the place of the item currently assigned if any.
+  local item_assigned_row, item_assigned_column, item_assigned_index
+  local item_assigned = game:get_item_assigned(1)
+  if item_assigned ~= nil then
+    local item_name_assigned = item_assigned:get_name()
+    for i, item_name in ipairs(inventory_items_names) do
+
+      if item_name == item_name_assigned then
+        item_assigned_column = (i - 1) % inventory_num_columns
+        item_assigned_row = math.floor((i - 1) / inventory_num_columns)
+        item_assigned_index = i - 1
       end
     end
   end
-end
 
-function inventory_manager:initialize_sprites_grid()
-  for i=1,num_rows do
-    self.sprites_grid[i] = {}
-    for j=1,num_columns do
-      self.sprites_grid[i][j] = nil
-      if (self.grid[i][j] ~= nil) then
-        self.sprites_grid[i][j] = sol.surface.create("items/inventory/".. self.grid[i][j] .. ".png")
+  -- Rapidly moves the inventory widgets towards or away from the screen.
+  local function move_widgets(callback)
+
+    local angle_added = 0
+    if select(2, inventory_widget:get_xy()) > 0 then
+      -- Opposite direction when closing.
+      angle_added = math.pi
+    end
+
+    local movement = sol.movement.create("straight")
+    movement:set_speed(movement_speed)
+    movement:set_max_distance(movement_distance)
+    movement:set_angle(3 * math.pi / 2 + angle_added)
+    inventory_widget:start_movement(movement, callback)
+
+    local movement = sol.movement.create("straight")
+    movement:set_speed(movement_speed)
+    movement:set_max_distance(movement_distance)
+    movement:set_angle(3 * math.pi / 2 + angle_added)
+    quest_widget:start_movement(movement)
+
+    local movement = sol.movement.create("straight")
+    movement:set_speed(movement_speed)
+    movement:set_max_distance(movement_distance)
+    movement:set_angle(3 * math.pi / 2 + angle_added)
+    do_widget:start_movement(movement)
+
+    local movement = sol.movement.create("straight")
+    movement:set_speed(movement_speed)
+    movement:set_max_distance(movement_distance)
+    movement:set_angle(3 * math.pi / 2 + angle_added)
+    equipment_widget:start_movement(movement)
+
+  end
+
+  local cursor_index = game:get_value("pause_inventory_last_item_index") or 0
+  local cursor_row = math.floor(cursor_index / inventory_num_columns)
+  local cursor_column = cursor_index % inventory_num_columns
+
+  -- Draws cursors on the selected and on the assigned items.
+  local function draw_item_cursors(dst_surface)
+
+    -- Selected item.
+    local widget_x, widget_y = inventory_widget:get_xy()
+    item_cursor_moving_sprite:draw(
+        dst_surface,
+        widget_x + 24 + 32 * cursor_column,
+        widget_y + 24 + 32 * cursor_row
+    )
+
+    -- Item assigned (only if different from the selected one).
+    if item_assigned_row ~= nil then
+      if item_assigned_index ~= cursor_index then
+        item_cursor_fixed_sprite:draw(
+            dst_surface,
+            widget_x + 24 + 32 * item_assigned_column,
+            widget_y + 24 + 32 * item_assigned_row
+        )
       end
     end
   end
-end
 
---This was written simply for debugging.
-function inventory_manager:print_grid()
-  for i=1,5 do
-    for j=1,2 do
-      print(self.grid[i][j])
+  -- Changes the position of the item cursor.
+  local function set_cursor_position(row, column)
+    cursor_row = row
+    cursor_column = column
+    cursor_index = cursor_row * inventory_num_columns + cursor_column
+    if cursor_index == item_assigned_index then
+      item_cursor_moving_sprite:set_animation("solid_blinking")
+      item_cursor_moving_sprite:set_frame(1)
+    else
+      item_cursor_moving_sprite:set_animation("dashed_blinking")
     end
   end
+
+  function inventory:on_draw(dst_surface)
+
+    inventory_widget:draw(dst_surface)
+    quest_widget:draw(dst_surface)
+    do_widget:draw(dst_surface)
+    equipment_widget:draw(dst_surface)
+
+    -- Show the item cursors.
+    draw_item_cursors(dst_surface)
+  end
+
+  function inventory:on_command_pressed(command)
+
+    if state ~= "ready" then
+      return true
+    end
+
+    local handled = false
+
+    if command == "pause" then
+      -- Close the pause menu.
+      state = "closing"
+      sol.audio.play_sound("pause_closed")
+      move_widgets(function() game:set_paused(false) end)
+      handled = true
+
+    elseif command == "item_1" or command == "action" then
+      -- Assign an item.
+      local item = game:get_item("inventory/" .. inventory_items_names[cursor_index + 1])
+      if cursor_index ~= item_assigned_index
+          and item:has_variant()
+          and item:is_assignable() then
+        sol.audio.play_sound("ok")
+        game:set_item_assigned(1, item)
+        item_assigned_row, item_assigned_column = cursor_row, cursor_column
+        item_assigned_index = cursor_row * inventory_num_rows + cursor_column
+        item_cursor_moving_sprite:set_animation("solid_blinking")
+        item_cursor_moving_sprite:set_frame(0)
+      end
+      handled = true
+
+    elseif command == "right" then
+      if cursor_column < inventory_num_columns - 1 then
+        sol.audio.play_sound("cursor")
+        set_cursor_position(cursor_row, cursor_column + 1)
+        handled = true
+      end
+
+    elseif command == "up" then
+      sol.audio.play_sound("cursor")
+      if cursor_row > 0 then
+        set_cursor_position(cursor_row - 1, cursor_column)
+      else
+        set_cursor_position(inventory_num_rows - 1, cursor_column)
+      end
+      handled = true
+
+    elseif command == "left" then
+      if cursor_column > 0 then
+        sol.audio.play_sound("cursor")
+        set_cursor_position(cursor_row, cursor_column - 1)
+        handled = true
+      end
+
+    elseif command == "down" then
+      sol.audio.play_sound("cursor")
+      if cursor_row < inventory_num_rows - 1 then
+        set_cursor_position(cursor_row + 1, cursor_column)
+      else
+        set_cursor_position(0, cursor_column)
+      end
+      handled = true
+    end
+
+    return handled
+  end
+
+  function inventory:on_finished()
+    -- Store the cursor position.
+    game:set_value("pause_inventory_last_item_index", cursor_index)
+  end
+
+  set_cursor_position(cursor_row, cursor_column)
+  move_widgets(function() state = "ready" end)
+
+  return inventory
 end
 
 return inventory_manager
+
